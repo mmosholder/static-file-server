d955a6a0d28c8b3f4e9ba035dd5ecf2a587b0e8a
Malinda Mosholder <m.m.mosholder@gmail.com>
2015-05-05 13:49:00 -0600

    finished all but bonus2

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
-app.get('/', function(req, res) {
-	res.render('index');    
+
+fs.readFile('data.txt', function (err, data) {
+    app.get('/', function(req, res) {
+	       
+        res.header('Content-Type', 'text/html');
+        res.send(data);
+    });
 });
+
 
 var server = app.listen(3122, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
 
-var fs = require('fs');
-var fileContents = fs.readFileSync('data.txt');
